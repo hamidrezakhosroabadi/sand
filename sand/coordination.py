bcca325cf718e3dbf173a502f1399412830eeb2d
Hamidreza Khosroabadi <80723130+hamidrezakhosroabadi@users.noreply.github.com>
2023-05-04 09:49:47 -0700

    Coordination class and methods were implemented.

---
+++ sand/coordination.py
@@ -0,0 +1,28 @@
+from kazoo.client import KazooClient
+
+
+DEFAULT_PATH = '/'
+
+
+class Coordinator():
+
+    def __init__(self, zookeeper_host, hostname):
+        self.__zookeeper_host = zookeeper_host
+        self.__hostname = hostname
+        self.__connection = KazooClient(hosts=self.__zookeeper_host)
+
+    def start(self):
+        self.__connection.start()
+        self.__connection.create(DEFAULT_PATH, self.__hostname, ephemeral=True)
+
+    def stop(self):
+        self.__connection.stop()
+
+    def exists(self, hostname):
+        return self.__connection.exists(DEFAULT_PATH+hostname)
+
+    def get_members(self):
+        return self.__connection.get_children(DEFAULT_PATH)
+
+    def __repr__(self):
+        return f'{type(self.__name__)}({self.__zookeeper_host!r},{self.last_name!r})'
