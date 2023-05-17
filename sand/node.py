8c6f1d4bd642498ec60be12754ec6755e1052d38
Hamidreza <hamidrezakhosroabadi@outlook.com>
2023-05-17 22:11:21 +0330

    Ring & Node class were refactord.

--- sand/node.py
+++ sand/node.py
@@ -1,16 +1,23 @@
 from requests import get, post, delete
+from hashlib import md5
+from functools import total_ordering
+from sys import byteorder
 
 
+@total_ordering
 class Node:
     def __init__(self, name, address):
         self.__name = name
         self.__address = address
 
-    def is_lock(self, key):
-        return True if get(f'{self.__address}/lock/{key}').status_code == 200 else False
+    def __hash__(self):
+        return int.from_bytes(md5(f'{self.__name}{self.__address}'.encode()).digest(), byteorder)
 
-    def lock(self, key):
-        post(f'{self.__address}/lock/{key}')
+    def __lt__(self, obj):
+        return hash(self) < hash(obj)
 
-    def unlock(self, key):
-        delete(f'{self.__address}/lock/{key}')
+    def __eq__(self, obj):
+        return hash(self) == hash(obj)
+
+    def __repr__(self):
+        return f'{type(self).__name__}({self.__name!r},{self.__address!r})'
--- sand/ring.py
+++ sand/ring.py
@@ -1,6 +1,23 @@
+from sortedcontainers import SortedSet
+from node import Node
+
+
 class Ring:
     def __init__(self, nodes):
-        self.__nodes = [*nodes]
+        self.__nodes = SortedSet(nodes)
 
-    def get_nodes(self):
-        return self.__nodes
+    def add(self, node):
+        self.__nodes.add(node)
+
+    def remove(self, node):
+        self.__nodes.remove(node)
+
+    def get(self, obj, replicas):
+        bisect_index = self.__nodes.bisect(obj)
+        return [self.__nodes[bisect_index+i] for i in range(replicas)]
+
+    def __len__(self):
+        return len(self.__nodes)
+
+    def __repr__(self):
+        return f'{type(self).__name__}({list(self.__nodes)!r})'
